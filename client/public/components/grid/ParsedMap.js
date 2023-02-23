7d2329ec8d9e0d4d4d9e565cbe0844d6bda79258
Javi Trek <j1a1b1m1@Gmail.com>
2023-02-23 11:41:00 -0500

    Start of Dijkstra's Algorithm
    Just starting Dijksta's algorithm to start our pathfinding function (fun). Comitting this small amount because Im moving from working on my desktop PC to laptop. Consequently, I need to have the code up in the repo so I can get it out on the laptop.

--- client/public/components/grid/ParsedMap.js
+++ client/public/components/grid/ParsedMap.js
@@ -14,7 +14,9 @@ export function ParsedMap() {
         axios.get('/map/parsedMap')
             .then(res => {
                 let mapTiles = []
+
                 res.data.forEach((tile, index) => {
+
                     mapTiles.push(
                         <div key={index} className={`mapTile c${tile[0]} r${tile[1]}`}>
                             <div className={`${tile[2].slice(0, 3)} `}>
@@ -30,6 +32,46 @@ export function ParsedMap() {
         });
     }, [])
 
+
+    // Determine our tiles
+    const startCoordinates = [7, 10]
+    const movement = 6
+
+    function possibleMovement(maxX, maxY, movement, start) {
+
+        //maxX maxY = rows columns, multiply together and get all tiles in grid
+        const nodeAmount = maxX * maxY
+
+        //lets create our grid!
+
+        //So we need to initialize our visited nodes (which we will use to check if we have used or haven't used the route before)
+        let visitedNodes = [];
+        // Our presumed distance from start to the given tile
+        let distance = [];
+
+        //our previous tiles?
+        let previous = [];
+
+        // the cost of movement in this tile?
+        let movementCost = [];
+
+        //lets actually build the grid
+        for (let i = 0; i < nodeAmount; i++) {
+            // we havent visited it yet
+            visitedNodes.push(false);
+            //?
+            distance.push(Infinity);
+            // we havent seen previous yet
+            previous.push(null);
+            // we still dont have the movement cost of each tile
+            movementCost.push(null);
+        }
+
+        let startTile = start[0] + start[1] * maxX
+        console.log(startTile)
+    }
+
+    possibleMovement(18, 18, 6, [1, 3])
 
     return (
 
