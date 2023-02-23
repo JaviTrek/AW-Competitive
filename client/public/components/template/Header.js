5cd72e0b0164d24b444bc065c0b77c40160f39f7
Miguel Garcia <112121391+MiguelGarcia2002@users.noreply.github.com>
2023-02-23 16:23:15 -0500

    This is the header branch, stil on WIP

--- client/public/components/template/Header.js
+++ client/public/components/template/Header.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+//import logo from './images/awLogo.webp';
 
 export const Header = () => {
   return (
     <div className='header'>
-      <h1 className='header__title'>Header</h1>
+      <div className='headerGrid'> 
+
+        <div>
+          <img className='pictureElement' src = "/images/awLogo.webpg" alt=""/>
+        </div>
+
+        <div className='menuElementGrid'>
+          <div className='menuElementGridButton'> Competition</div>
+          <div className='menuElementGridButton'> How to play</div>
+          <div className='menuElementGridButton'> Community</div>
+          <div className='menuElementGridButton'> Tools</div>
+        </div>
+
+        <div className = 'loginContainer'>
+          <div className='menuElementGridLogin'> Login</div>
+        </div>
+
+      </div>
+
     </div>
+
+    
   )
 }
