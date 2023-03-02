e6e97d3c09c8edd0b04ff70c1b93f6dfac0eb470
Rey Jairus Marasigan <reymarasigan001@gmail.com>
2023-03-01 21:08:20 -0500

    Finished the current games page
    Also made the SimpleUsername component customizable, where icon and gradient changes depending on the color prop passed in. Hope you don't mind the flexboxes that I used >.<

--- client/public/components/CurrentGames.js
+++ client/public/components/CurrentGames.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../style/CurrentGames.sass'
+import { SimpleUsername } from './gameInterface/SimpleUsername'
 
 export const CurrentGames = () => {
   return (
@@ -15,16 +16,16 @@ export const CurrentGames = () => {
           </div>
           <div className='CGInfo'>
             <h2 className='CGTitle'>GL STD [T1]: hoochinutz vs Femboy</h2>
-            <h3 className='CGSubtitle'>Day 13: 06:23:22 until Clock Expires</h3>
+            <h3 className='CGSubtitle'><span className='CGSubtitleDay'>Day 13</span>: 06:23:22 until Clock Expires</h3>
             <h4 className='CGMapName'>Caustic Finale</h4>
           </div>
           <div className='CGCharacter CGCharacter2'>
             <img src="../images/samiChar.webp" alt="character 2"/>
           </div>
           <div className='CGUsers'>
-            <h3>Femboy</h3>
+            <SimpleUsername name="Femboy" color="blue"/>
             <img className="VSImage" src="../images/vs.png" alt="vs"/>
-            <h3>Mipin</h3>
+            <SimpleUsername name="Mipin" color="red"/>
           </div>
         </article>
       </div>
