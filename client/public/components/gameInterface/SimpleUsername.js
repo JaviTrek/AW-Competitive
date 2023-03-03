aa0ec99960b8a75bd1039e663d656ad6a3ef0263
Rey Jairus Marasigan <reymarasigan001@gmail.com>
2023-03-03 16:24:40 -0500

    Added character icons and minor changes
    Character icons are in images/CO. Refactored SimpleUsername.js by removing the object and adding redCountry and blueCountry classes.

--- client/public/components/gameInterface/SimpleUsername.js
+++ client/public/components/gameInterface/SimpleUsername.js
@@ -1,25 +1,13 @@
 import React from 'react'
 import '../../style/gameInterface/simpleUsername.sass'
 
-const usernameColors = {
-  "red": {
-    gradient: "linear-gradient(90deg, rgba(255, 0, 0, 0.84) 0%, #690000 100%)",
-    icon: "../../images/oslogo1.png"
-  },
-  "blue": {
-    gradient: "linear-gradient(90deg, rgba(35, 66, 186, 0.84) 0%, #0B2070 100%)",
-    icon: "../../images/oslogo2.png"
-  }
-}
-
 export const SimpleUsername = ({name="name", color = "red"}) => {
   // color specifies what gradient the background of the username will be
-
-  const style = { "background": usernameColors[color]["gradient"]};
+  // valid colors = blue, red
   return (
-    <div className="usernameContainer" style={style}>
+    <div className={`usernameContainer ${usernameClassColors[color]}Country`}>
       <h3>{name}</h3>
-      <img className="userContainerIcon" src={usernameColors[color]["icon"]}/>
+      <img className="userContainerIcon"/>
     </div>
   )
 }
