216f417f696b6631e245e4556bf3c49e90a8d3f1
Emanuel Rivas <Emanuelrivasdavid@gmail.com>
2023-03-07 10:38:27 -0500

    Created currentplayer skeleton
    Created the skeleton structure of the currentplayer component
    Created the sass file for the currentPlayer component and added the same background colors as the figma

--- client/public/components/currentPlayerInfo/CurrentPlayer.js
+++ client/public/components/currentPlayerInfo/CurrentPlayer.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import '../../style/currentPlayer.sass'
+import IMG from '../../images/maxChar.webp'
+
+
 
 const CurrentPlayer = () => {
   return (
-    <div className='test'>
-      HELLOOOO
+    <div className='current-player'>
+      <div className='header'>
+        <div className='header-background'>
+          <div className='character-background'>xxx</div>
+        </div>
+      </div>
+      <div className='player-name'>3</div>
+      <div className='player-stats'>
+        4
+      </div>
     </div>
   )
 }
