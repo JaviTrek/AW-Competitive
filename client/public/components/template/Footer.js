9f634784a4e2111ec742e06df0c12618bfc05284
Miguel Garcia <112121391+MiguelGarcia2002@users.noreply.github.com>
2023-03-07 15:06:43 -0500

    Added the tiny bottom padding, and changed GH logo

--- client/public/components/template/Footer.js
+++ client/public/components/template/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import awLogo from '../../images/awLogo.webp'
-import ghLogo from '../../images/footerImages/GitHub-Mark.png'
+import ghLogo from '../../images/footerImages/GitHub.png'
 import rLogo from '../../images/footerImages/Reddit.png'
 import dLogo from '../../images/footerImages/Discord.png'
 
@@ -13,22 +13,22 @@ export const Footer = () => {
            <img className ='imageLogo' src ={awLogo} alt=""/>
         </div>
 
-        <div className = 'menuElementGridF'>  
+        <nav className = 'menuElementGridF'>  
           <div className = 'menuButtonElementGridF'> Home </div>
           <div className = 'menuButtonElementGridF'> About us </div>
           <div className = 'menuButtonElementGridF'> Terms of Use </div>
           <div className = 'menuButtonElementGridF'> Rules </div>
-        </div>
+        </nav>
 
-        <div className = 'iconElements'>
+        <nav className = 'iconElements'>
           <img className = 'iconButtonElementGridRedditF'src ={rLogo} alt=""/>
           <img className = 'iconButtonElementGridDiscordF'src ={dLogo} alt=""/>
           <img className = 'iconButtonElementGridGhF'src ={ghLogo} alt=""/>
-        </div>
+        </nav>
 
         <div className = 'horizontalLine'> </div>
 
-        <div className = 'copyrightElement'> Advance Wars is (c) 1990-2001 Nintendo and (c) 2001 Intelligent Systems. All images are copyright their respective owners.</div>
+        <p className = 'copyrightElement'> Advance Wars is (c) 1990-2001 Nintendo and (c) 2001 Intelligent Systems. All images are copyright their respective owners.</p>
       </div>
       
     </div>
