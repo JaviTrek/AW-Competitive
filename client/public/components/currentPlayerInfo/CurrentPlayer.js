95da8bebdb26c595a78c68da75e27965ac2d4bda
Emanuel Rivas <Emanuelrivasdavid@gmail.com>
2023-03-12 17:03:53 -0400

    Created CurrentPlayer Grid
    Created full layout for current player component
    Added Styling to make it  *MOSTLY* responsive
    Image still needs has issues on smaller screens

--- client/public/components/currentPlayerInfo/CurrentPlayer.js
+++ client/public/components/currentPlayerInfo/CurrentPlayer.js
@@ -10,8 +10,21 @@ export const CurrentPlayer = () => {
         <img className="character-image" src={IMG} />
         <div className="character-background"></div>
       </div>
-      <div className="player-name">{NameBanner}</div>
-      <div className="player-stats">4</div>
+      <div className="player-name">
+        <NameBanner name="Emasaur" color="blue" />
+      </div>
+      <div className="player-stats">
+        <div className="user-info">
+          <div>3</div>
+          <div>4</div>
+          <div className="test">YOO</div>
+        </div>
+        <div className="army-stats">
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+        </div>
+      </div>
     </div>
   );
 };
