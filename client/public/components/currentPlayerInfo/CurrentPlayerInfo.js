14671e9190e4252533a08e077f12dffc1470d5d8
Emanuel Rivas <Emanuelrivasdavid@gmail.com>
2023-03-20 12:21:54 -0400

    Created mini "database"
    -Created an array called currentPlayerData containing dummy values for current player stats like playerBalance, userName, etc.
    
    -This is to set up and simulate how the CurrentPlayer component would receive data from the actual database

---
+++ client/public/components/currentPlayerInfo/CurrentPlayerInfo.js
@@ -0,0 +1,22 @@
+export const currentPlayerData = [
+  {
+    selectedCharacter: "../images/CO/Max-Full.png",
+    characterBackground: "character-background-blue",
+    userName: "Emasaur",
+    timePlayed: "33:10:21",
+    playerBalance: "5000",
+    armyCount: "32",
+    armyValue: "114100",
+    income: "21000",
+  },
+  {
+    selectedCharacter: "../images/CO/Sami-Full.png",
+    characterBackground: "character-background-red",
+    userName: "Emasaur",
+    timePlayed: "33:10:21",
+    playerBalance: "5000",
+    armyCount: "32",
+    armyValue: "114100",
+    income: "21000",
+  },
+];
