23b7064b8a60be29dff2986e0a0e029368133f20
Emanuel Rivas <Emanuelrivasdavid@gmail.com>
2023-03-22 13:50:23 -0400

    Added destructuring to CurrentPlayer component
    - Added parameters to the currentPlayer component using destructuring to get player information from a database instead of just using static values.

--- client/public/components/currentPlayerInfo/CurrentPlayer.js
+++ client/public/components/currentPlayerInfo/CurrentPlayer.js
@@ -1,28 +1,29 @@
 import React from "react";
 import "../../style/currentPlayer.sass";
-import IMG from "../../images/maxChar.webp";
+import IMG from "../../images/CO/Max-Full.png";
 import { NameBanner } from "../gameInterface/NameBanner";
 
-export const CurrentPlayer = () => {
+
+export const CurrentPlayer = ({selectedCharacter,characterBackground,characterImagePosition,userName}) => {
   return (
     <div className="current-player">
       <div className="current-player-header">
-        <img className="character-image" src={IMG} />
-        <div className="character-background"></div>
+        <img className={characterImagePosition} src={selectedCharacter}/>
+        <div className={characterBackground}></div>
       </div>
       <div className="player-name">
-        <NameBanner name="Emasaur" color="blue" />
+        <NameBanner name={userName} color="blue" />
       </div>
       <div className="player-stats">
         <div className="user-info">
-          <div>3</div>
-          <div>4</div>
-          <div className="test">YOO</div>
+          <div>33:10:21</div>
+          <div>4000</div>
+          <div className="exp-bar">Placeholder for EXP Bar</div>
         </div>
         <div className="army-stats">
-          <div>5</div>
-          <div>6</div>
-          <div>7</div>
+          <div>32</div>
+          <div>114100</div>
+          <div>21000</div>
         </div>
       </div>
     </div>
