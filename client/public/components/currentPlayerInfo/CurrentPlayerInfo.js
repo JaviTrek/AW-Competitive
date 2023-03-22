041faff8aff36c032103d937bda6cbca04c14bfb
Emanuel Rivas <Emanuelrivasdavid@gmail.com>
2023-03-22 13:48:21 -0400

    Updated values for CurrentPlayerInfo
    - Added characterImage value to be able to position the picked character's image depending on if the player is Player1 or Player2

--- client/public/components/currentPlayerInfo/CurrentPlayerInfo.js
+++ client/public/components/currentPlayerInfo/CurrentPlayerInfo.js
@@ -2,6 +2,7 @@ export const currentPlayerData = [
   {
     selectedCharacter: "../images/CO/Max-Full.png",
     characterBackground: "character-background-blue",
+    characterImage: "character-image-player1",
     userName: "Emasaur",
     timePlayed: "33:10:21",
     playerBalance: "5000",
@@ -12,7 +13,8 @@ export const currentPlayerData = [
   {
     selectedCharacter: "../images/CO/Sami-Full.png",
     characterBackground: "character-background-red",
-    userName: "Emasaur",
+    characterImage: "character-image-player2",
+    userName: "XMontu",
     timePlayed: "33:10:21",
     playerBalance: "5000",
     armyCount: "32",
