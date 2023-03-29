6afa626b8c9f6cac54c3e9a04974612d364755a5
Rey Jairus Marasigan <reymarasigan001@gmail.com>
2023-03-29 16:41:51 -0400

    formatting login page

--- client/public/components/LoginForm.js
+++ client/public/components/LoginForm.js
@@ -1,91 +1,93 @@
-import React, {useState} from "react";
-import {SmallContainer} from "./template/Container";
+import React, { useState } from "react";
+import { SmallContainer } from "./template/Container";
 import styles from "../style/Form.module.sass";
-import '../style/abstracts/flashMessage.sass'
-import {useForm} from "react-hook-form";
-import {useNavigate} from "react-router-dom";
+import "../style/abstracts/flashMessage.sass";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export function Login() {
-    const navigate = useNavigate();
-    const {register, handleSubmit, watch} = useForm({
-        defaultValues: {
-            username: "", password: ""
-        },
-    });
+  const navigate = useNavigate();
+  const { register, handleSubmit, watch } = useForm({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
 
+  const [flash, setFlash] = useState({
+    class: "none",
+    message: "",
+  });
 
-    const [flash, setFlash] = useState({
-        class: "none", message: "",
-    })
-
-
-// <form method='post' action="/loginUser" className="authenticationForm">
-    return (<SmallContainer title="Log In">
-        <div className={`flashMessage ${flash.class}Flash`}>
-            {flash.message}
-        </div>
-        <form method="get" action="/routes/auth">
-            <button
-                tabIndex={0}
-                className={`btn ${styles.discordButton}`}
-                type="submit"
-            >
-                Continue with Discord
-            </button>
-        </form>
-
-        <div className={styles.orDivider}>
-            <div className={styles.orLine}/>
-            <p>Or</p>
-            <div className={styles.orLine}/>
-        </div>
-
-        <form
-            className="authenticationForm"
-            onSubmit={handleSubmit((data) => {
-                axios.post("/loginUser", null, {
-                    params: data,
-                })
-                    .then((res) => {
-                        setFlash({
-                            class: "success", message: "You have logged in correctly! Redirecting..."
-                        })
-                        //give the user some time to read success message
-                        setTimeout(() => {
-                            navigate("/")
-                        }, 3000)
-                    })
-                    .catch((err) => {
-                        console.error(err)
-                        setFlash({
-                            class: "error", message: "Your username or password was incorrect, please try again!"
-                        })
-                    });
-
-
-            })}
+  // <form method='post' action="/loginUser" className="authenticationForm">
+  return (
+    <SmallContainer title="Log In">
+      <div className={`flashMessage ${flash.class}Flash`}>{flash.message}</div>
+      <form method="get" action="/routes/auth">
+        <button
+          tabIndex={0}
+          className={`btn ${styles.discordButton}`}
+          type="submit"
         >
-            <input
-                className={styles.input}
-                {...register("username")}
-                type="text"
-                placeholder="Username"
-                required
-            />
-            <input
-                className={`${styles.input} ${styles.inputPassword}`}
-                {...register("password")}
-                type="password"
-                placeholder="Password"
-                required
-            />
-            <p className={styles.subtext}>
-                New to Advance Wars? <a href="/register">Sign Up</a>
-            </p>
-            <button tabIndex={0} className={`btn ${styles.formBtn}`} type="submit">
-                Login
-            </button>
-        </form>
-    </SmallContainer>);
+          Continue with Discord
+        </button>
+      </form>
+
+      <div className={styles.orDivider}>
+        <div className={styles.orLine} />
+        <p>Or</p>
+        <div className={styles.orLine} />
+      </div>
+
+      <form
+        className="authenticationForm"
+        onSubmit={handleSubmit((data) => {
+          axios
+            .post("/loginUser", null, {
+              params: data,
+            })
+            .then((res) => {
+              setFlash({
+                class: "success",
+                message: "You have logged in correctly! Redirecting...",
+              });
+              //give the user some time to read success message
+              setTimeout(() => {
+                navigate("/");
+              }, 3000);
+            })
+            .catch((err) => {
+              console.error(err);
+              setFlash({
+                class: "error",
+                message:
+                  "Your username or password was incorrect, please try again!",
+              });
+            });
+        })}
+      >
+        <input
+          className={styles.input}
+          {...register("username")}
+          type="text"
+          placeholder="Username"
+          required
+        />
+        <input
+          className={`${styles.input} ${styles.inputPassword}`}
+          {...register("password")}
+          type="password"
+          placeholder="Password"
+          required
+        />
+        <p className={styles.subtext}>
+          New to Advance Wars? <a href="/register">Sign Up</a>
+        </p>
+        <button tabIndex={0} className={`btn ${styles.formBtn}`} type="submit">
+          Login
+        </button>
+      </form>
+    </SmallContainer>
+  );
 }
