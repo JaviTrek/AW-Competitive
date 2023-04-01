e05e5a0c38b8331ee1aa40339d6a6b14a2604d20
JaviTrek <j1a1b1m1@gmail.com>
2023-04-01 10:31:04 -0400

    Fix main crashing
    We were missing a } and a ]

--- client/public/index.js
+++ client/public/index.js
@@ -1,72 +1,73 @@
-
 import React, {StrictMode} from 'react';
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Template } from "./components/template/Template";
-import { App } from "./components/App";
-import { Grid } from "./components/grid/Grid";
-import { About } from "./components/About";
-import { AppChildren } from "./components/AppChildren";
-import { CurrentGames } from "./components/CurrentGames";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Template} from "./components/template/Template";
+import {App} from "./components/App";
+import {Grid} from "./components/grid/Grid";
+import {About} from "./components/About";
+import {AppChildren} from "./components/AppChildren";
+import {CurrentGames} from "./components/CurrentGames";
 
-import { Match } from "./components/Match";
-import { NewGame } from "./components/NewGame";
-import { createRoot } from "react-dom/client";
-import { Register } from "./components/RegisterForm";
-import { Login } from "./components/LoginForm";
+import {Match} from "./components/Match";
+import {NewGame} from "./components/NewGame";
+import {createRoot} from "react-dom/client";
+import {Register} from "./components/RegisterForm";
+import {Login} from "./components/LoginForm";
 
 
 const container = document.getElementById("app");
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Template />,
-    children: [
-      {
-        name: "App",
+    {
         path: "/",
-        element: <App />,
-      },
-      {
-        name: "children",
-        path: "children",
-        element: <AppChildren />,
-      },
-      {
-        name: "about",
-        path: "about",
-        element: <About />,
-      },
-      {
-        name: "game",
-        path: "game",
-        element: <Grid />,
-      },
-      {
-        name: "currentgames",
-        path: "currentgames",
-        element: <CurrentGames />,
-      },
-      {
+        element: <Template/>,
+        children: [
+            {
+                name: "App",
+                path: "/",
+                element: <App/>,
+            },
+            {
+                name: "children",
+                path: "children",
+                element: <AppChildren/>,
+            },
+            {
+                name: "about",
+                path: "about",
+                element: <About/>,
+            },
+            {
+                name: "game",
+                path: "game",
+                element: <Grid/>,
+            },
+            {
+                name: "currentgames",
+                path: "currentgames",
+                element: <CurrentGames/>,
+            },
+            {
 
-        name: "match",
-        path: "match",
-        element: <Match />,
-      },
-
-        name: "newgame",
-        path: "newgame",
-        element: <NewGame />,
-      },
-  {
-    path: "register",
-    element: <Register />,
-  },
-  {
-    path: "login",
-    element: <Login />,
-  },
+                name: "match",
+                path: "match",
+                element: <Match/>,
+            },
+            {
+                name: "newgame",
+                path: "newgame",
+                element: <NewGame/>,
+            },
+            {
+                path: "register",
+                element: <Register/>,
+            },
+            {
+                path: "login",
+                element: <Login/>,
+            },
+        ]
+    }
 ]);
 
 const root = createRoot(container);
-root.render(<RouterProvider router={router} />);
+root.render(<RouterProvider router={router}/>);
