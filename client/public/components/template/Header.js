8ab3196280d67f44f0cd511f54966edd713f36f0
JaviTrek <j1a1b1m1@gmail.com>
2023-04-13 14:45:14 -0400

    About page by Zach

--- client/public/components/template/Header.js
+++ client/public/components/template/Header.js
@@ -40,7 +40,7 @@ export const Header = () => {
         <a className="headerNav headerMenuButton" href={'/currentgames'}> Current games</a>
         <a className="headerNav headerMenuButton" href={'/newGame'}> Start a game</a>
           <a className="headerNav headerMenuButton" href={'/startGames'}> Join a game</a>
-        <a className="headerNav headerMenuButton"> How to play</a>
+        <a className="headerNav headerMenuButton" href={'/howtoplay'}> How to play</a>
       </nav>
 
       {login}
